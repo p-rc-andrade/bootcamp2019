b92e333b871502a307293864a643bddb25c48370
p-rc-andrade <46648202+p-rc-andrade@users.noreply.github.com>
2019-08-21 17:16:57 +0100

    Add modulo01; Add .gitignore

---
+++ modulo01/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const server = express();
+
+// Query params = ?test=1
+// Route params = /users/1
+// request body = {"name": "Pedro", "email": "pedro.andrade@sbanken.no"}
+
+server.get("/users/:id", (req, res) => {
+  //   const name = req.query.name;
+  const { id } = req.params;
+  //   return res.send("Hey");
+  return res.json({ message: `Buscando o usuário ${id}` });
+});
+
+server.listen(3333);
